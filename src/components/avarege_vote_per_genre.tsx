e6fe903a286f30a53bcb788937cf129a422543e5
GabrielUzel <gabriel.uzel1@gmail.com>
2025-05-13 23:25:59 -0300

    Fixed lint error

--- src/components/avarege_vote_per_genre.tsx
+++ src/components/avarege_vote_per_genre.tsx
@@ -1,4 +1,4 @@
-import styles from "../styles/avarege_vote_per_genre.module.css";
+// import styles from "../styles/avarege_vote_per_genre.module.css";
 
 export default function AvarageVotePerGenre() {
     return (
