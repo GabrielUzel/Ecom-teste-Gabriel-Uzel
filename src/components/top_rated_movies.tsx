67d209f1379ec665952def9b65967431d136b7c2
GabrielUzel <gabriel.uzel1@gmail.com>
2025-05-14 14:37:05 -0300

    Fix api key

--- src/components/top_rated_movies.tsx
+++ src/components/top_rated_movies.tsx
@@ -38,7 +38,7 @@ export default function TopRatedMovies() {
                     method: 'GET',
                     headers: {
                         accept: 'application/json',
-                        Authorization: `Bearer ${process.env.API_KEY}`
+                        Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`
                     }
                 };
 
