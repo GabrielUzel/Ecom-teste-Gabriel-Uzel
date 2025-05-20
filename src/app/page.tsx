4ffab343d9be1adacd3cd518b5a9d52516001a8f
GabrielUzel <gabriel.uzel1@gmail.com>
2025-05-20 13:54:31 -0300

    Improved CLS performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
             </p>
             <div className={styles.horizontal_line}></div>
 
-            <div>
+            <div style={{ minHeight: '400px' }}>
                 <h2 className={styles.home_subtitle}>Top 250 filmes</h2>
                 {isLoadingTop ? <Loading /> : <TopRatedMovies data={topMovies ?? []} />}
             </div>
@@ -77,22 +77,22 @@ export default function Home() {
             <div>
                 <h2 className={styles.home_subtitle}>Métricas</h2>
                 <div className={styles.metrics_container}>
-                    <div>
+                    <div style={{ minHeight: '100px' }}>
                         <h3 className={styles.metrics_subtitle}>Média de nota por gênero</h3>
                         <AvaregeVotePerGenre data={topMovies ?? []} genresData={genres ?? []} isLoading={isLoadingTop || isLoadingGenres}/>
                     </div>
-                    <div>
+                    <div style={{ minHeight: '100px' }}>
                         <h3 className={styles.metrics_subtitle}>Quantidade de filmes por gênero</h3>
                         <MoviesPerGenre data={topMovies ?? []} genresData={genres ?? []} isLoading={isLoadingTop || isLoadingGenres}/>
                     </div>
-                    <div>
+                    <div style={{ minHeight: '100px' }}>
                         <h3 className={styles.metrics_subtitle}>Quantidade de filmes por ano</h3>
                         {isLoadingTop ? <Loading /> : <MoviesPerYear data={topMovies ?? []}/>}
                     </div>
                 </div>
             </div>
             <div className={styles.horizontal_line}></div>
-            <div>
+            <div style={{ minHeight: '400px' }}>
                 <h2 className={styles.home_subtitle}>Filmes no top trending da semana</h2>
                 {isLoadingTrending ? <Loading /> : <TrendingMovies topMoviesData={topMovies ?? []} trendingData={trendingMovies ?? []}/>} 
             </div>
